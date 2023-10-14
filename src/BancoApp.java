fd0538190981639e50e631cbb753c1cf2e8c5437
gabrielfernandesgf <djgabrielfernandes01@gmail.com>
2023-10-14 17:32:56 -0300

    BancoApp

---
+++ src/BancoApp.java
@@ -0,0 +1,32 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class BancoApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+        
+   
+        double valorInicial = scanner.nextDouble();
+        
+     
+        double taxaJuros = scanner.nextDouble();
+        
+        int periodo = scanner.nextInt();
+        
+        double valorFinal = calcularJuros(valorInicial, taxaJuros, periodo);
+
+        
+        //TODO: Iterar, baseado no período em anos, para calculo do valorFinal com os juros.
+
+
+        System.out.println("Valor final do investimento: R$ " + df.format(valorFinal));
+        
+        scanner.close();
+    
+    }
+
+    public static double calcularJuros(double valorFinal, double taxaJuros, int periodo){
+        return valorFinal * Math.pow(1 + taxaJuros, periodo);
+    }
+}
